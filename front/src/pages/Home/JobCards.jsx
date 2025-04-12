4becdb875bfc99e39e53068ec6bec9ccdbfba933
ImangaliR <k.imangali.r@gmail.com>
2025-04-13 03:34:15 +0500

    Added: Project Catalog page

---
+++ front/src/pages/Home/JobCards.jsx
@@ -0,0 +1,107 @@
+import warn from "../../assets/warn.png";
+import chatgreen from "../../assets/chatgreen.png";
+
+const languageColors = {
+  0: "#FFF27F",
+  1: "#DE9DFC",
+  2: "#A0E7E5",
+  3: "#FFC75F",
+  4: "#F9A1BC",
+  5: "#55EAD7",
+  6: "#55AAFF",
+  7: "#FDCBFA",
+  8: "#9EE493",
+  9: "#AECBFE",
+};
+
+const JobCards = ({ job }) => {
+  function formatDate(dateString) {
+    const options = {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour12: true,
+    };
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", options).replace(",", " -");
+  }
+
+  return (
+    <>
+      <p className="text-[#9eff92]"></p>
+      <div className="bg-white shadow-sm rounded-lg p-10">
+        <h1 className="text-lg font-bold">{job?.title}</h1>
+        <div className="flex items-center justify-between mb-6">
+          <h2 className="flex items-center text-[#949494] gap-2 font-semibold ml-3">
+            <p>{formatDate(job?.startDate)}, </p>
+            <p>{formatDate(job?.endDate)}</p>
+          </h2>
+          <p className="bg-[#05E400] text-white px-6 py-1 text-center rounded-sm">
+            New
+          </p>
+        </div>
+        <div className="w-full mb-6">
+          <div className="flex items-baseline justify-between gap-8">
+            <div className="flex items-baseline gap-4">
+              <h1 className="text-lg font-bold">Language</h1>
+              <div className="grid grid-cols-5 gap-2">
+                {job?.languages?.map((language, i) => (
+                  <p
+                    key={i}
+                    className={`text-[#585858] px-3 py-0.5 rounded-md text-center`}
+                    style={{ backgroundColor: languageColors[i] }}
+                  >
+                    {language?.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <h1 className="top-0 text-lg font-bold">Price: {job?.price}</h1>
+          </div>
+        </div>
+        <div className="mb-6">
+          <h1 className="text-lg font-bold mb-1">Description</h1>
+          <p
+            className={`ml-3  px-4 py-3 ${
+              job?.description ? "bg-[#EAEAEA]" : ""
+            }`}
+          >
+            {job?.description}
+          </p>
+        </div>
+        <div className="flex items-baseline mb-6">
+          <h1 className="text-lg font-bold">Specialization: </h1>
+          <div className="ml-2 grid grid-cols-3 gap-2 w-fit">
+            {job?.specializations?.map((specialization, i) => (
+              <p key={i} className="text-[#196FD3] w-fit">
+                #{specialization?.name}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-baseline mb-6">
+          <h1 className="text-lg font-bold">Service Types: </h1>
+          <div className="ml-2 grid grid-cols-2 gap-2 w-fit">
+            {job?.serviceTypes?.map((serviceType, i) => (
+              <p key={i} className="text-[#196FD3] w-fit">
+                #{serviceType?.name}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center justify-end gap-2">
+          <button className="text-[#D8000D] border-3 rounded-sm font-semibold flex items-center justify-center gap-2 w-30 h-9">
+            Report
+            <img src={warn} alt="warn icon" className="w-4 h-4 mt-1" />
+          </button>
+          <button className="text-[#2A9E97] border-3 rounded-sm font-semibold flex items-center justify-center gap-2 w-30 h-9">
+            Chat
+            <img src={chatgreen} alt="chat icon" className="w-4 h-4 mt-1" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JobCards;
--- front/src/pages/Home/Project_Catalog.jsx
+++ front/src/pages/Home/Project_Catalog.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../components/layout/Navbar";
+import Search from "./../../components/Search/Search";
+import JobCards from "./JobCards";
+import api from "../../services/api";
+import { toast } from "react-toastify";
 
 const ProjectCatalog = () => {
+  const [jobCount, setJobCount] = useState(0);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await api.get("/jobs/search?title=");
+        setJobs(response?.data?.data);
+        setJobCount(response?.data?.data.length);
+      } catch (err) {
+        toast.error("Failed to fetch jobs.");
+      }
+    };
+
+    fetchJobs();
+  }, []);
   return (
     <>
       <Navbar />
-      <h1>This is Project Catalog page</h1>
+      <div className="grid justify-center">
+        <Search />
+        <div className="my-10">
+          <main className="w-220 lg:w-260">
+            <p className="text-2xl mb-1">Results ({jobCount})</p>
+            <div className="grid grid-cols-1 gap-4">
+              {jobs.map((job, i) => (
+                <JobCards key={i} job={job} />
+              ))}
+            </div>
+          </main>
+        </div>
+      </div>
     </>
   );
 };
