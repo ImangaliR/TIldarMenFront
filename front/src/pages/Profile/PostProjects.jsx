19e27405d14cc820843697bad46a83ad4022c173
ImangaliR <k.imangali.r@gmail.com>
2025-04-30 23:03:25 +0500

    Fixed: ui

--- front/src/pages/Profile/PostProjects.jsx
+++ front/src/pages/Profile/PostProjects.jsx
@@ -58,68 +58,82 @@ const PostProjects = () => {
                 New Project
               </button>
             </div>
-            <div className="overflow-x-auto">
-              <table className="min-w-full divide-y divide-gray-200 text-sm">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      Project Title
-                    </th>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      Start Date
-                    </th>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      End Date
-                    </th>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      Published Date
-                    </th>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      Price
-                    </th>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      Number of Applicants
-                    </th>
-                    <th className="px-4 py-2 text-center font-semibold text-gray-600">
-                      Actions
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-100">
-                  {projects?.map((proj, idx) => (
-                    <tr
-                      key={idx}
-                      onClick={() => navigate(`project-applicants/:${proj.id}`)}
-                      className={`hover:bg-blue-50 ${
-                        idx === 4 ? "bg-blue-50" : ""
-                      }`}
-                    >
-                      <td className="px-4 py-2 text-center">{proj.title}</td>
-
-                      <td className="px-4 py-2 text-center">
-                        {formatDate(proj.startDate)}
-                      </td>
-                      <td className="px-4 py-2 text-center">
-                        {formatDate(proj.endDate)}
-                      </td>
-                      <td className="px-4 py-2 text-center text-blue-600">
-                        {formatDateRange(proj.publicationDate)}
-                      </td>
-                      <td className="px-4 py-2 text-center">{proj.price}</td>
-                      <td className="px-4 py-2 text-center">
-                        {proj.applicantsCount}
-                      </td>
-                      <td className="px-4 py-2 text-gray-400 text-center cursor-pointer">
-                        <button className="flex items-center gap-1 text-[#38BF4C]">
-                          Edit
-                          <img src={pen} alt="pen image" className="w-3 h-3" />
-                        </button>
-                      </td>
+            {projects?.length !== 0 ? (
+              <div className="overflow-x-auto">
+                <table className="min-w-full divide-y divide-gray-200 text-sm">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        Project Title
+                      </th>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        Start Date
+                      </th>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        End Date
+                      </th>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        Published Date
+                      </th>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        Price
+                      </th>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        Number of Applicants
+                      </th>
+                      <th className="px-4 py-2 text-center font-semibold text-gray-600">
+                        Actions
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  </thead>
+                  <tbody className="divide-y divide-gray-100">
+                    {projects?.map((proj, idx) => (
+                      <tr
+                        key={idx}
+                        onClick={() =>
+                          navigate(`project-applicants/:${proj.id}`)
+                        }
+                        className={`hover:bg-blue-50 ${
+                          idx === 4 ? "bg-blue-50" : ""
+                        }`}
+                      >
+                        <td className="px-4 py-2 text-center">{proj.title}</td>
+
+                        <td className="px-4 py-2 text-center">
+                          {formatDate(proj.startDate)}
+                        </td>
+                        <td className="px-4 py-2 text-center">
+                          {formatDate(proj.endDate)}
+                        </td>
+                        <td className="px-4 py-2 text-center text-blue-600">
+                          {formatDateRange(proj.publicationDate)}
+                        </td>
+                        <td className="px-4 py-2 text-center">{proj.price}</td>
+                        <td className="px-4 py-2 text-center">
+                          {proj.applicantsCount}
+                        </td>
+                        <td className="px-4 py-2 text-gray-400 text-center cursor-pointer">
+                          <button className="flex items-center gap-1 text-[#38BF4C]">
+                            Edit
+                            <img
+                              src={pen}
+                              alt="pen image"
+                              className="w-3 h-3"
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <div className="flex items-center justify-center mt-50">
+                <p className="text-[#8b8b8b] text-3xl">
+                  No existing projects yet
+                </p>
+              </div>
+            )}
           </div>
         </div>
       )}
