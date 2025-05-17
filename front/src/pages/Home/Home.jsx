936a2dbad32572610cf57489147a7e5e1667ccf9
ImangaliR <k.imangali.r@gmail.com>
2025-05-17 23:56:35 +0500

    Added: ui to home page

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -6,6 +6,17 @@ import rectangles from "../../assets/rectangles.png";
 import chel from "../../assets/chel.png";
 import arrow from "../../assets/arrow.png";
 import arrowdown from "../../assets/arrowdown.png";
+import rightarrow from "../../assets/rightarrow.png";
+import business from "../../assets/business.png";
+import sales from "../../assets/sales.png";
+import overall from "../../assets/overall.png";
+import finance from "../../assets/finance.png";
+import engineering from "../../assets/engineering.png";
+import technology from "../../assets/technology.png";
+import instagram from "../../assets/instagram.png";
+import facebook from "../../assets/facebook.png";
+import linkedin from "../../assets/linkedin.png";
+import twitter from "../../assets/twitter.png";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
 import { useEffect, useState } from "react";
@@ -100,23 +111,183 @@ export const Home = () => {
             <div className="w-100 h-50 absolute bottom-0 right-0 bg-white [clip-path:polygon(100%_0,100%_100%,0_100%)] z-3"></div>
           </div>
         </div>
-        <div className="px-30 bg-white">
-          <div className="pt-20 flex items-center justify-between">
-            <h1 className="text-4xl font-bold">
+        <div className="px-30 py-20 bg-white">
+          <div className="flex items-center justify-between">
+            <h1 className="text-5xl font-bold">
               Explore by <span className="text-[#26A4FF]">specializations</span>
             </h1>
             <div
               className="flex items-center gap-3 cursor-pointer"
               onClick={() => navigate("/project-catalog")}
             >
-              <p className="text-[#4640DE] text-lg font-semibold">
+              <p className="text-[#4640DE] text-xl font-semibold">
                 Show all projects
               </p>
               <img src={arrow} alt="arrow icon" className="w-5 h-5" />
             </div>
           </div>
+          <div className="grid grid-cols-4 gap-5 mt-12">
+            <div className="flex flex-col justify-center p-6 w-74 h-50 border-2 border-[#D6DDEB]">
+              <img
+                src={overall}
+                alt="pen and ruler icon"
+                className="w-10 h-10"
+              />
+              <h1 className="text-2xl font-bold py-2">Overall</h1>
+              <div className="flex items-center gap-3 cursor-pointer">
+                <p className="text-[#7C8493] pb-1">Available projects</p>
+                <img
+                  src={rightarrow}
+                  alt="arrow right icon"
+                  className="w-5 h-5"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center p-6 w-74 h-50 border-2 border-[#D6DDEB]">
+              <img src={sales} alt="sales icon" className="w-10 h-10" />
+              <h1 className="text-2xl font-bold py-2 text-nowrap">
+                Marketing Translation
+              </h1>
+              <div className="flex items-center gap-3 cursor-pointer">
+                <p className="text-[#7C8493] pb-1">Available projects</p>
+                <img
+                  src={rightarrow}
+                  alt="arrow right icon"
+                  className="w-5 h-5"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center p-6 w-74 h-50 border-2 border-[#D6DDEB]">
+              <img src={finance} alt="money icon" className="w-10 h-10" />
+              <h1 className="text-2xl font-bold py-2 text-nowrap">
+                Financial Translation
+              </h1>
+              <div className="flex items-center gap-3 cursor-pointer">
+                <p className="text-[#7C8493] pb-1">Available projects</p>
+                <img
+                  src={rightarrow}
+                  alt="arrow right icon"
+                  className="w-5 h-5"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center p-6 w-74 h-50 border-2 border-[#D6DDEB]">
+              <img src={technology} alt="monitor icon" className="w-10 h-10" />
+              <h1 className="text-2xl font-bold py-2 text-nowrap">
+                Technical Translation
+              </h1>
+              <div className="flex items-center gap-3 cursor-pointer">
+                <p className="text-[#7C8493] pb-1">Available projects</p>
+                <img
+                  src={rightarrow}
+                  alt="arrow right icon"
+                  className="w-5 h-5"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center p-6 w-74 h-50 border-2 border-[#D6DDEB]">
+              <img src={engineering} alt="code icon" className="w-10 h-10" />
+              <h1 className="text-2xl font-bold py-2 text-nowrap">
+                Software Localization
+              </h1>
+              <div className="flex items-center gap-3 cursor-pointer">
+                <p className="text-[#7C8493] pb-1">Available projects</p>
+                <img
+                  src={rightarrow}
+                  alt="arrow right icon"
+                  className="w-5 h-5"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center p-6 w-74 h-50 border-2 border-[#D6DDEB]">
+              <img src={business} alt="business icon" className="w-10 h-10" />
+              <h1 className="text-2xl font-bold py-2 text-nowrap">
+                Diplomatic Translation
+              </h1>
+              <div className="flex items-center gap-3 cursor-pointer">
+                <p className="text-[#7C8493] pb-1">Available projects</p>
+                <img
+                  src={rightarrow}
+                  alt="arrow right icon"
+                  className="w-5 h-5"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </main>
+      <footer className="bg-[#202430] text-white px-30 py-10">
+        <div className="flex justify-between gap-10 text-[#D6DDEB] mt-5">
+          <div>
+            <h1 className="text-2xl font-bold text-white">TildarMen</h1>
+            <p className="w-100 py-5">
+              A platform that connects passionate translators with clients who
+              need clear, professional, and multilingual communication.
+            </p>
+          </div>
+          <div className="flex gap-30">
+            <div>
+              <h1 className="text-lg font-bold text-white">About</h1>
+              <p className="py-5 cursor-pointer">Companies</p>
+              <p className="cursor-pointer">Pricing</p>
+              <p className="py-5 cursor-pointer">Terms</p>
+              <p className="cursor-pointer">Advice</p>
+              <p className="py-5 cursor-pointer">Privacy Policy</p>
+            </div>
+            <div>
+              <h className="text-lg font-bold text-white">Recources</h>
+              <p className="py-5 cursor-pointer">Help Docs</p>
+              <p className="cursor-pointer">Guide</p>
+              <p className="py-5 cursor-pointer">Updates</p>
+              <p className="cursor-pointer">Contact Us</p>
+            </div>
+            <div>
+              <h1 className="text-lg font-bold text-white">
+                Get job notifications
+              </h1>
+              <p className="w-65 py-5">
+                The latest project news, articles, sent to your inbox weekly.
+              </p>
+              <div className="flex items-center gap-3 mt-4">
+                <input
+                  type="text"
+                  placeholder="Email Address"
+                  className="bg-white px-4 py-2 focus:outline-none"
+                />
+                <button className="font-bold bg-[#4640DE] text-white px-5 py-2">
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="border-white border-t-2 my-10 w-full opacity-10" />
+        <div className="flex items-center justify-between">
+          <h1 className="opacity-50">2025 @ TildarMen. All rights reserved.</h1>
+          <div className="flex items-center gap-5">
+            <img
+              src={facebook}
+              alt="icon"
+              className="w-10 h-10 cursor-pointer"
+            />
+            <img
+              src={instagram}
+              alt="icon"
+              className="w-10 h-10 cursor-pointer"
+            />
+            <img
+              src={linkedin}
+              alt="icon"
+              className="w-10 h-10 cursor-pointer"
+            />
+            <img
+              src={twitter}
+              alt="icon"
+              className="w-10 h-10 cursor-pointer"
+            />
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
